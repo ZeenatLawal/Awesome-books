d4ab14693d008c3867f3ab0f51641791c7240559
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-08-09 11:09:05 +0100

    Create function to add books

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const form = document.getElementById('booksForm');
+let myBooks = [];
+
+function addBook() {
+  const book = {
+    id: myBooks.length,
+    title: document.getElementById('bookName').value,
+    author: document.getElementById('authorName').value,
+  };
+  myBooks.push(book);
+  console.log(myBooks);
+  localStorage.setItem('myBooks', JSON.stringify(myBooks));
+  form.reset();
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBook();
+});
