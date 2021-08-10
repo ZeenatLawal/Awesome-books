7a486b4e625b77bd9ab0fe0cbf572efa5a6cb013
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-08-10 10:49:26 +0100

    Add remove books method to the class

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const form = document.getElementById('booksForm');
 const display = document.getElementById('display');
 
-// let myBooks = [];
-
-// function showBooks(books) {
-//   const listBook = books.map((b) => `<li> ${b.title} by ${b.author} <button id='${b.id}' type='button' class="removeBtn">Remove Book</button></li>`).join('');
-//   display.innerHTML = `${listBook}`;
-// }
-
 // function removeBook(ev) {
 //   const buttonId = ev.target.id;
 //   myBooks = myBooks.filter(
@@ -18,56 +11,45 @@ const display = document.getElementById('display');
 //   localStorage.setItem('myBooks', JSON.stringify(myBooks));
 //   showBooks(myBooks);
 // }
-
-// function addBook() {
-//   const book = {
-//     id: myBooks.length,
-//     title: document.getElementById('bookName').value,
-//     author: document.getElementById('authorName').value,
-//   };
-//   myBooks.push(book);
-//   localStorage.setItem('myBooks', JSON.stringify(myBooks));
-//   if (myBooks.length > 0) {
-//     showBooks(myBooks);
-//   }
-//   display.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('removeBtn')) {
-//       removeBook(e);
-//     }
-//   });
-//   form.reset();
-// }
-
-
 class Books {
   constructor() {
     this.myBooks = [];
   }
 
- showBooks(books) {
-  const listBook = books.map((b) => `<li> ${b.title} by ${b.author} <button id='${b.id}' type='button' class="removeBtn">Remove Book</button></li>`).join('');
-  display.innerHTML = `${listBook}`;
-}
+  removeBook(ev) {
+    const buttonId = ev.target.id;
+    this.myBooks = this.myBooks.filter(
+      (y) => y !== this.myBooks[this.myBooks.findIndex(
+        (x) => x.id === parseInt(buttonId, 10),
+      )],
+    );
+    localStorage.setItem('myBooks', JSON.stringify(this.myBooks));
+    this.showBooks(this.myBooks);
+  }
+
+  showBooks(books) {
+    const listBook = books.map((b) => `<li> ${b.title} by ${b.author} <button id='${b.id}' type='button' class="removeBtn">Remove Book</button></li>`).join('');
+    display.innerHTML = `${listBook}`;
+  }
 
   addBook() {
-  const book = {
-    id: this.myBooks.length,
-    title: document.getElementById('bookName').value,
-    author: document.getElementById('authorName').value,
-  };
-  this.myBooks.push(book);
-  localStorage.setItem('myBooks', JSON.stringify(this.myBooks));
-  if (this.myBooks.length > 0) {
-   this.showBooks(this.myBooks);
+    const book = {
+      id: this.myBooks.length,
+      title: document.getElementById('bookName').value,
+      author: document.getElementById('authorName').value,
+    };
+    this.myBooks.push(book);
+    localStorage.setItem('myBooks', JSON.stringify(this.myBooks));
+    if (this.myBooks.length > 0) {
+      this.showBooks(this.myBooks);
+    }
+    display.addEventListener('click', (e) => {
+      if (e.target.classList.contains('removeBtn')) {
+        this.removeBook(e);
+      }
+    });
+    form.reset();
   }
-  // display.addEventListener('click', (e) => {
-  //   if (e.target.classList.contains('removeBtn')) {
-  //     removeBook(e);
-  //   }
-  // });
-  form.reset();
-}
-
 }
 
 const awesomeBook = new Books();
